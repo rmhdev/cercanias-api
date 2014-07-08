e5680bd29d61d6cc8d01ba6fc4be6654cc3dbe4e
Rober Martín H <rmh.dev@gmail.com>
2014-07-08 19:24:45 +0200

    Development environment.

---
+++ src/env_dev.php
@@ -0,0 +1,9 @@
+<?php
+
+require __DIR__ . "/../vendor/autoload.php";
+
+/* @var Silex\Application $app */
+$app = require __DIR__ . "/app.php";
+$app["debug"] = true;
+
+return $app;
---
+++ web/index_dev.php
@@ -0,0 +1,5 @@
+<?php
+
+/* @var Silex\Application $app */
+$app = require __DIR__ . "/../src/env_dev.php";
+$app->run();
