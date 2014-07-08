fbf01e8512fb608ef48abef7a8610f91cc3e7e16
Rober Martín H <rmh.dev@gmail.com>
2014-07-08 19:24:24 +0200

    API homepage shows urls for calls.

--- src/controllers.php
+++ src/controllers.php
@@ -3,5 +3,16 @@
 /* @var Silex\Application */
 
 $app->get('/', function () use ($app) {
-    return $app->json(array());
-});
+    return $app->json(array(
+        "route_url"     => "http://localhost/route",
+        "timetable_url" => "http://localhost/timetable",
+    ));
+})->bind("homepage");
+
+$app->get('/route', function () use ($app) {
+    return "";
+})->bind("route");
+
+$app->get('/timetable', function () use ($app) {
+    return "";
+})->bind("timetable");
--- tests/CercaniasApi/Tests/IndexTest.php
+++ tests/CercaniasApi/Tests/IndexTest.php
@@ -26,4 +26,19 @@ class IndexTest extends WebTestCase
         $this->assertTrue($response->isSuccessful());
         $this->assertEquals("application/json", $response->headers->get("Content-Type"));
     }
+
+    public function testIndexPageShowsUrls()
+    {
+        $client = $this->createClient();
+        $client->request("GET", "/");
+        $response = $client->getResponse();
+
+        $jsonExpected = array(
+            "route_url"     => "http://localhost/route",
+            "timetable_url" => "http://localhost/timetable",
+        );
+        $jsonResponse = json_decode($response->getContent(), true);
+
+        $this->assertEquals($jsonExpected, $jsonResponse);
+    }
 }
