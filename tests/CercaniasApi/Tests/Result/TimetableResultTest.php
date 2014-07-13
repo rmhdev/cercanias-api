d24640a954f80a85e32e9238820dfc76f75f3798
Rober Martín H <rmh.dev@gmail.com>
2014-07-13 19:06:08 +0200

    Add test file.

---
+++ tests/CercaniasApi/Tests/Result/TimetableResultTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace CercaniasApi\Tests;
+
+use Cercanias\Entity\Station;
+use Cercanias\Entity\Timetable;
+use CercaniasApi\Result\TimetableResult;
+
+class TimetableResultTest extends \PHPUnit_Framework_TestCase
+{
+    public function testSimpleTimetableToArray()
+    {
+        $timetable = new Timetable(
+            new Station("111", "Departure station", 1),
+            new Station("222", "Destination station", 1)
+        );
+        $result = new TimetableResult($timetable);
+        $data = $result->toArray();
+
+        $expectedDeparture = array(
+            "id" => "111", "name" => "Departure station", "route_id" => 1
+        );
+        $this->assertEquals($expectedDeparture, $data["departure"]);
+    }
+}
