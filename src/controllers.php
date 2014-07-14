6e7cd3a4088c5ff464876da2db9813ea64e3510c
Rober Martín H <rmh.dev@gmail.com>
2014-07-14 20:18:35 +0200

    Refactor controller.

--- src/controllers.php
+++ src/controllers.php
@@ -20,9 +20,9 @@ $app->get('/route', function () use ($app) {
     return $app->json($result->toArray());
 })->bind("route_list");
 
-$app->get('/route/{routeId}', function ($routeId) use ($app) {
+$app->get('/route/{routeId}', function (Request $request) use ($app) {
     $result = new \CercaniasApi\Result\RouteResult(
-        $app["cercanias"]->getRoute((int) $routeId)
+        $app["cercanias"]->getRoute((int) $request->get("routeId"))
     );
 
     return $app->json($result->toArray());
@@ -30,12 +30,12 @@ $app->get('/route/{routeId}', function ($routeId) use ($app) {
 
 $app->get(
     '/timetable/{routeId}/{departureId}/{destinationId}',
-    function ($routeId, $departureId, $destinationId) use ($app) {
+    function (Request $request) use ($app) {
         $query = new \Cercanias\Provider\TimetableQuery();
         $query
-            ->setRoute((int) $routeId)
-            ->setDeparture($departureId)
-            ->setDestination($destinationId)
+            ->setRoute(         (int) $request->get("routeId"))
+            ->setDeparture(     $request->get("departureId"))
+            ->setDestination(   $request->get("destinationId"))
         ;
         $result = new \CercaniasApi\Result\TimetableResult(
             $app["cercanias"]->getTimetable($query)
